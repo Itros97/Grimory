31c718082d056a1b3e1534d9f55d34af0054915e
Itros97 <itros97@gmail.com>
2025-10-04 16:08:03 +0200

    Added enemy and loop

---
+++ Mobs/soldier_a.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+@export var speed: float = 500.0
+
+func _process(delta: float) -> void:
+	var pf := get_parent() as PathFollow2D
+	if pf == null:
+		return
+
+	pf.progress += speed * delta 
+
+	if pf.progress_ratio >= 0.999:
+		pf.progress_ratio = 1.0
+		queue_free()
